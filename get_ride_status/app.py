22fed036d00154f64f7356a06b094a393df189a2
Neil Daryl Sulit <znrsulit@globe.com.ph>
2020-06-20 16:44:07 +0000

    Add Get Ride Functionality & Modify Book Ride

--- get_ride_status/app.py
+++ get_ride_status/app.py
@@ -2,7 +2,7 @@ import json
 import boto3
 import os
 import uuid
-import datetime
+import datetime as dt
 
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key #, Attr
@@ -17,16 +17,25 @@ table = client.Table(os.environ["RIDES_TABLE"])
 def lambda_handler(event, context):
     params = event.get("pathParameters")
     rideId = params.get('rideId')
+    
     if rideId:
         result = table.query(
-            ProjectionExpression='ride_status',
             KeyConditionExpression=Key('ride_id').eq(rideId)
         )
-        response = result['Items'][0] if len(result['Items']) > 0 else 'Ride Not Found.'
+        
+        if result['Items'][0] and len(result['Items']) > 0:
+            datenow = dt.datetime.now()
+            timestamp_obj = dt.datetime.strptime(result['Items'][0]['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+            if timestamp_obj + dt.timedelta(seconds=120) < datenow and result['Items'][0]['ride_status'] == 'Booked':
+                response = 'Booking Expired.'
+            else:
+                response = result['Items'][0]['ride_status']
+        else:
+            response = 'Ride Not Found.'
     else:
         response = { 'error': 'Please provide a Ride ID.' }
 
     return {
         "statusCode": 200,
-        "body": json.dumps(response),
+        "body": json.dumps({'ride_status': response}),
     }
