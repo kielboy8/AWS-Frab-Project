e491bdaa88b42aae2748d413a5c730798e09214a
Neil Daryl Sulit <znrsulit@globe.com.ph>
2020-06-24 09:14:13 +0000

    Add ElastiCache to SAM

--- accept_ride_request/app.py
+++ accept_ride_request/app.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+import datetime 
 
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key #, Attr
@@ -10,44 +11,60 @@ client = boto3.resource(
    region_name=os.environ['DEFAULT_REGION'],
 )
 
+sqs = boto3.client('sqs', os.environ["BOOKING_QUEUE"])
 table = client.Table(os.environ["RIDES_TABLE"])
 table2 = client.Table(os.environ["DRIVERS_TABLE"])
+table3  = client.Table(os.environ["LOCATIONS_TABLE"])
 
 def lambda_handler(event, context):
-    driverId = event.get('driverId')
-    rideId = event.get('rideId')
+    params = event.get("pathParameters")
+    driverId = params.get('driverId')
+    rideId = params.get('rideId')
     
-    update_ride_table=table.update_item(
-        Key={
-            'ride_id': rideId
-        },
-        ExpressionAttributeNames={
-            '#RS': 'ride_status',
-        },
-        ExpressionAttributeValues={
-            ':s': 'Found a driver',
-        },
-        UpdateExpression="set #RS=:s",
-    )
+    body = json.loads(event['body'])
+    acceptLocation = json.loads(body['acceptLocation'])
     
-    update_driver_table=table2.update_item(
-        Key={
-            'driver_id': driverId
-        },
-        ExpressionAttributeNames={
-            '#DS': 'driver_status',
-        },
-        ExpressionAttributeValues={
-            ':s': 'Booked to a ride',
-        },
-        UpdateExpression="set #DS=:s",
-    )
+    if driverId and rideId and acceptLocation:
+        dateNow = str(datetime.datetime.now().isoformat())
+        update_ride_table=table.update_item(
+            Key={
+                'ride_id': rideId
+            },
+            ExpressionAttributeNames={
+                '#RS': 'ride_status',
+                '#AA': 'accepted_at',
+                '#AL': 'accepted_location'
+            },
+            ExpressionAttributeValues={
+                ':s': 'accepted',
+                ':aa': dateNow,
+                ':al': str(acceptLocation)
+            },
+            UpdateExpression="set #RS=:s, #AA=:aa, #AL=:al",
+        )
+        
+        update_driver_table=table2.update_item(
+            Key={
+                'driver_id': driverId
+            },
+            ExpressionAttributeNames={
+                '#DS': 'current_ride',
+                '#CL': 'current_location'
+            },
+            ExpressionAttributeValues={
+                ':s': rideId,
+                ':cc': str(acceptLocation),
+            },
+            UpdateExpression="set #DS=:s",
+        )
+        
+        #Delete Message from Queue
     
     return {
         "statusCode": 200,
         "body": json.dumps({
             "rideId": rideId,
-            "driverId": driverId,
+            "acceptLocation": str(acceptLocation),
             "rideStatus": 'Found a driver',
         }),
     }
