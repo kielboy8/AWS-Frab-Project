052a7e20da6b2acf72a162ef639e431c3d6efbbd
Ubuntu <ubuntu@ip-172-31-10-73.ap-southeast-1.compute.internal>
2020-06-24 14:29:42 +0000

    added redis to requirements and commented redis code

--- drivers_location/app.py
+++ drivers_location/app.py
@@ -2,11 +2,13 @@ import json
 import boto3
 import os
 from faker import Faker
+import redis
 
 # import requests
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DRIVERS_TABLE'])
+# redis_endpoint = redis.Redis(host=os.environ['ELASTICACHE_HOST'], port=os.environ['ELASTICACHE_PORT'], db=0)
 
 def lambda_handler(event, context):
     params = event.get('pathParameters')
@@ -21,6 +23,15 @@ def lambda_handler(event, context):
         }
     )
     
+    # redis_endpoint.geoadd('rides', '121.0463828', '14.5543191', 'Globe Telecom')
+    
+    # result = redis_endpoint.geopos('rides', 'Globe Telecom')
+    
+    # redis_result_body = {
+    #     'N': result[0][0],
+    #     'W': result[0][1]
+    # }
+    
     responseBody = {
         'N': requestBody['updatedLocation']['N'],
         'W': requestBody['updatedLocation']['W']
@@ -31,6 +42,7 @@ def lambda_handler(event, context):
         "body": json.dumps({
             "driverId": driverId,
             "updatedLocation": responseBody,
+            # "location_response": redis_result_body
             # "location": ip.text.replace("\n", "")
         }),
     }
