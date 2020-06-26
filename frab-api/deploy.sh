7311ecee4a1ebbb562309c045994f24070636c2e
Neil Daryl Sulit <znrsulit@globe.com.ph>
2020-06-26 18:15:44 +0000

    Remove Cloned frab-api

--- frab-api/deploy.sh
+++
@@ -1,1 +0,0 @@
-sam build && sam package --output-template-file packaged.yaml --s3-bucket frab-api-artifact && sam deploy --template-file packaged.yaml --capabilities CAPABILITY_IAM --stack-name frab-api
