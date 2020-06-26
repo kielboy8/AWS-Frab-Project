5e60af9c72e3523adbacb0208d6180cb9f8a015b
Neil Daryl Sulit <znrsulit@globe.com.ph>
2020-06-26 14:01:03 +0000

    Add Location Ride State Updaters

--- drivers_location/app.py
+++ drivers_location/app.py
@@ -4,47 +4,71 @@ import os
 import redis
 from uuid import uuid4
 
-# import requests
-
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['DRIVERS_TABLE'])
-redis_endpoint = redis.Redis(host=os.environ['ELASTICACHE_HOST'], port=os.environ['ELASTICACHE_PORT'], db=0)
+driversTbl = dynamodb.Table(os.environ['DRIVERS_TABLE'])
+ridesTbl = dynamodb.Table(os.environ['RIDES_TABLE'])
+r = redis.Redis(host=os.environ['ELASTICACHE_HOST'], port=os.environ['ELASTICACHE_PORT'], 
+     charset='utf-8', decode_responses=True, db=0)
 
 def lambda_handler(event, context):
     params = event.get('pathParameters')
     driverId = params.get('driverId')
     requestBody = json.loads(event.get('body'))
     
-    driverLocId = None
+    driverLocId = str(uuid4().hex)
     
     try:
-        driverLocId = table.get_item(
+        driverLocId = driversTbl.get_item(
             Key={
                 'driver_id': driverId
             },
             ProjectionExpression='location_id'
         )['Item']['location_id']
     except:
-        driverLocId = str(uuid4())
-        response = table.put_item(
+        response = driversTbl.put_item(
             Item={
                 'driver_id': driverId,
                 'location_id': driverLocId 
             }
         )
     
-    redis_endpoint.geoadd(
+    r.geoadd(
         'driversGeo', 
         requestBody['updatedLocation']['N'], 
         requestBody['updatedLocation']['W'], 
         driverId
     )
     
+    #Check if has current ride in cache
+    currentRideId = r.get('driverBooking:'+driverId)
+    if currentRideId:
+        currentRide = r.hgetall('bookingHash:'+currentRideId)
+        willExpire = False
+        if json.loads(currentRide['bookingLocation']) == requestBody['updatedLocation']:
+            if currentRide['state'] == 'accepted':
+                #Move to In-Progress
+                currentRide['state'] = 'in_progress'
+            elif currentRide['state'] == 'in_progress':
+                #Finish Ride
+                currentRide['state'] == 'complete'
+            
+            ridesTbl.update_item(
+                Key={
+                    'ride_id': currentRideId
+                },
+                UpdateExpression="set ride_status = :r",
+                ExpressionAttributeValues={
+                    ':r': currentRide['state'],
+                },
+            )
+            
+            r.hmset('bookingHash:'+currentRideId, currentRide)
+            willExpire and r.expire('bookingHash:'+currentRideId, 120)
+        
     return {
         "statusCode": 200,
         "body": json.dumps({
             "locationId": driverLocId,
             "updatedLocation": requestBody['updatedLocation']
-            # "location": ip.text.replace("\n", "")
         }),
     }
