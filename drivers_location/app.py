dd87890df066c0db873e4e33479eb73e5999fc01
Neil Daryl Sulit <znrsulit@globe.com.ph>
2020-06-26 15:12:31 +0000

    Add Distance, Add Fix on Locations Update

--- drivers_location/app.py
+++ drivers_location/app.py
@@ -14,61 +14,65 @@ def lambda_handler(event, context):
     params = event.get('pathParameters')
     driverId = params.get('driverId')
     requestBody = json.loads(event.get('body'))
-    
+    response = 'Invalid Input.'
+   
     driverLocId = str(uuid4().hex)
     
-    try:
-        driverLocId = driversTbl.get_item(
-            Key={
-                'driver_id': driverId
-            },
-            ProjectionExpression='location_id'
-        )['Item']['location_id']
-    except:
-        response = driversTbl.put_item(
-            Item={
-                'driver_id': driverId,
-                'location_id': driverLocId 
-            }
-        )
-    
-    r.geoadd(
-        'driversRidersGeo', 
-        requestBody['updatedLocation']['N'], 
-        requestBody['updatedLocation']['W'], 
-        driverId
-    )
-    
-    #Check if has current ride in cache
-    currentRideId = r.get('driverBooking:'+driverId)
-    if currentRideId:
-        currentRide = r.hgetall('bookingHash:'+currentRideId)
-        willExpire = False
-        if json.loads(currentRide['bookingLocation']) == requestBody['updatedLocation']:
-            if currentRide['state'] == 'accepted':
-                #Move to In-Progress
-                currentRide['state'] = 'in_progress'
-            elif currentRide['state'] == 'in_progress':
-                #Finish Ride
-                currentRide['state'] == 'complete'
-            
-            ridesTbl.update_item(
+    if requestBody and requestBody.get('updatedLocation'):
+        try:
+            driverLocId = driversTbl.get_item(
                 Key={
-                    'ride_id': currentRideId
+                    'driver_id': driverId
                 },
-                UpdateExpression="set ride_status = :r",
-                ExpressionAttributeValues={
-                    ':r': currentRide['state'],
-                },
+                ProjectionExpression='location_id'
+            )['Item']['location_id']
+        except:
+            driversTbl.put_item(
+                Item={
+                    'driver_id': driverId,
+                    'location_id': driverLocId 
+                }
             )
-            
-            r.hmset('bookingHash:'+currentRideId, currentRide)
-            willExpire and r.expire('bookingHash:'+currentRideId, 120)
+        
+        r.geoadd(
+            'driversRidersGeo', 
+            requestBody['updatedLocation']['W'],
+            requestBody['updatedLocation']['N'],
+            driverId
+        )
+        
+        #Check if has current ride in cache
+        currentRideId = r.get('driverBooking:'+driverId)
+        if currentRideId:
+            currentRide = r.hgetall('bookingHash:'+currentRideId)
+            willExpire = False
+            if json.loads(currentRide['bookingLocation']) == requestBody['updatedLocation']:
+                if currentRide['state'] == 'accepted':
+                    #Move to In-Progress
+                    currentRide['state'] = 'in_progress'
+                elif currentRide['state'] == 'in_progress':
+                    #Finish Ride
+                    currentRide['state'] == 'complete'
+                
+                ridesTbl.update_item(
+                    Key={
+                        'ride_id': currentRideId
+                    },
+                    UpdateExpression="set ride_status = :r",
+                    ExpressionAttributeValues={
+                        ':r': currentRide['state'],
+                    },
+                )
+                
+                r.hmset('bookingHash:'+currentRideId, currentRide)
+                willExpire and r.expire('bookingHash:'+currentRideId, 120)
+                
+        response = {
+            "locationId": driverLocId,
+            "updatedLocation": requestBody['updatedLocation']
+        }
         
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "locationId": driverLocId,
-            "updatedLocation": requestBody['updatedLocation']
-        }),
+        "body": json.dumps(response),
     }
